5c53eb3e6166cc60b4ae687591336a5b7463a122
Lokijopu <kguo2134@gmail.com>
2018-10-14 22:50:32 -0400

    Add files via upload

--- DiceDoubles.java
+++ DiceDoubles.java
@@ -9,15 +9,18 @@ public class DiceDoubles {
 		int count = 0;
 		DiceClass die1 = new DiceClass();
 		DiceClass die2 = new DiceClass();
-		while (die1.roll() != die2.roll()) {
-			if (true) {
+		int a = die1.roll();
+		int b = die2.roll();
+		while (a != b) {
 			count++;
-			System.out.println(die1.roll() + "  " + die2.roll());
-			die1.roll();
-			die2.roll();
-			}
+			System.out.println(a + " " + b);
+			a = die1.roll();
+			b = die2.roll();
 		}
-		
+		if (a == b) {
+			System.out.println(a + "=" + b);
+			count++;
+		}
 		System.out.println("Num rolls: " + count);
 	}
 
--- DiceTarget.java
+++ DiceTarget.java
@@ -3,9 +3,37 @@
  * @author Kevin Guo Period 6 DiceTarget
  *
  */
+import java.util.Scanner;
 public class DiceTarget {
 
 	public static void main (String args[]) {
+		int count = 0;
+		Scanner in = new Scanner(System.in);
+		DiceClass die1 = new DiceClass();
+		DiceClass die2 = new DiceClass();
+		int a = die1.roll();
+		int b = die2.roll();
+		int sum = a+b;
+		System.out.print("Input a target value between 2 and 12: ");
+		int target = in.nextInt();
+		while (target < 2 || target > 12) {
+			System.out.println("Error: Invalid input.");
+			System.out.print("Input a target value between 2 and 12: ");
+			target = in.nextInt();
+		} 
+			while (sum != target) {
+				count++;
+				System.out.println(a + " + " + b + " = " + sum + ". Num rolls: " + count);
+				a = die1.roll();
+				b = die2.roll();
+				sum = a + b;
+			} 
+			if (sum == target) {
+				count++;
+				System.out.println(a + " + " + b + " = " + sum + ". Num rolls: " + count);
+				System.out.println("Target has been reached.");
+			}
 		
+		System.out.println(count);
 	}
 }
